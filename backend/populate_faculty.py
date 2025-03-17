84ca11a6313ccf76504d13028a34d7c0c5b301af
SaiVarshithM <117940405+SaiVarshithMallavarapu@users.noreply.github.com>
2025-03-17 14:48:48 +0530

    populated

--- backend/populate_faculty.py
+++ backend/populate_faculty.py
@@ -6,34 +6,24 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')  # Replace '
 django.setup()
 
 from home.models import Faculty  # Replace 'your_app' with the app where the Faculty model is defined
-
-# List of faculty data to add
-faculty_data = [
-    {"name": "Vijay A. S", "role": "Member", "email": "vijay_as@iiti.ac.in", "location": "", "image": None},
-    {"name": "Sumit Gautam", "role": "Member", "email": "sumit.gautam@iiti.ac.in", "location": "", "image": None},
-    {"name": "Pravarthana Dhanapal", "role": "Member", "email": "dpravarthana@iiti.ac.in", "location": "", "image": None},
-    {"name": "Jayaprakash Murugesan", "role": "Member", "email": "jayaprakash@iiti.ac.in", "location": "", "image": None},
-    {"name": "Eswara Prasad Korimilli", "role": "Member", "email": "eswar@iiti.ac.in", "location": "", "image": None},
-    {"name": "Dr. Onkar Game", "role": "Member", "email": "ogame@iiti.ac.in", "location": "", "image": None},
-    {"name": "Dr. Narendranath Patra", "role": "Member", "email": "naren@iiti.ac.in", "location": "", "image": None},
-    {"name": "Dr. Gourab Sil", "role": "Member", "email": "gourabsil@iiti.ac.in", "location": "", "image": None},
-    {"name": "Dr. Dan Sathiaraj", "role": "Member", "email": "dansathiaraj@iiti.ac.in", "location": "", "image": None},
-    {"name": "Dr. Ayan Mondal", "role": "Member", "email": "ayanm@iiti.ac.in", "location": "", "image": None},
-    {"name": "Devendra Laxmanrao Deshmukh", "role": "Member", "email": "dldeshmukh@iiti.ac.in", "location": "", "image": None},
-    {"name": "Ashish Rajak", "role": "Member", "email": "a.rajak@iiti.ac.in", "location": "", "image": None},
-    {"name": "Anand Petare", "role": "Member", "email": "acpetare@iiti.ac.in", "location": "", "image": None},
+# List of Teaching Assistants
+tas = [
+    "Avani Ravindra Wanjari", "Rajat Vishwakarma", "Tejal Ravindra Uplenchwar", "Ankit Kumar Gupta", 
+    "VEMPATI RISHI", "Vardhan Santosh Bhalgat", "V Purushothaman", "Akasapu Chandrika nagavalli", 
+    "Divyam Pandey", "Tejas Chaudhari", "Sameer Lakkad", "Arruri Sathwik", "Akshit Raizada", 
+    "Ishita Umredkar", "Anant Sonekar", "Amara Venkata Ram Charan", "Aditya Suwalka", "Bhawna Chaudhary", 
+    "GIRIDHAR A P", "Mahak Kokate", "Jugal Shah", "Gaurav", "Gouriveni Gokul", "Renu Bhagat", "Tannu", 
+    "Kumari Sunita", "Vennela", "Arjun S Nair", "Soham Mondal", "Shashank Sharma", "Mihir Hemani", 
+    "Khushi Rohilla", "Manvendra Tripathi"
 ]
 
-# Add faculty data to the database
-for faculty in faculty_data:
-    obj, created = Faculty.objects.get_or_create(
-        name=faculty['name'],
-        role=faculty['role'],
-        email=faculty['email'],
-        location="IIT INDORE",
-        image=faculty['image']
+
+# Populate the Faculty model
+for name in tas:
+    Faculty.objects.get_or_create(
+        name=name,
+        category='TA',
+        year=2023  # Only for TAs
     )
-    if created:
-        print(f"Added: {faculty['name']}")
-    else:
-        print(f"Skipped: {faculty['name']} (already exists)")
+
+print("Teaching Assistants added successfully!")
